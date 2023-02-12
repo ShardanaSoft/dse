c8e1e851870b69e1fc684f6cafa456590d934a1b
shardanasoft <shardanasoft@gmail.com>
2023-02-12 10:22:12 +0100

    all ok?

--- packages/react/lib/molecules/Select/Select.js
+++ packages/react/lib/molecules/Select/Select.js
@@ -121,7 +121,7 @@ const Select = ({ onOptionSelected: handler, label = "Select an option", options
                 isSelected && (React.createElement("svg", { width: "1rem", xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", strokeWidth: 1.5, stroke: "currentColor" },
                     React.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", d: "M4.5 12.75l6 6 9-13.5" })))));
         }))));
-};
+}; //test comments for git
 
 export { Select as default };
 //# sourceMappingURL=Select.js.map
